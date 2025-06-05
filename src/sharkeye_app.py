376ff9c4ef3744b144f15fb2243f136eaea38e10
Lucas Joseph <lsjoseph42@gmail.com>
2025-06-05 15:59:14 -0700

    fix crash when one frame in track

--- src/sharkeye_app.py
+++ src/sharkeye_app.py
@@ -28,6 +28,7 @@ import shutil
 import tempfile
 import io
 import math
+from pathlib import Path
 from segmentation.segmentation_model import run_prediction, calculate_shark_length_from_pixel, find_pixel_length, draw_mask
 from segment_anything import sam_model_registry, SamPredictor 
 
@@ -253,7 +254,7 @@ class CustomTracker:
                     # Use segmentation model to generate lengths
                     mask = run_prediction(longest_frame, (int(x - w/2), int(y - h/2), int(x + w/2), int(y + h/2)))
                     pixel_length = find_pixel_length(mask, draw_line=False, viz_name = f'{video_name}-viz')
-                    segmentation_length = calculate_shark_length_from_pixel(pixel_length)
+                    segmentation_length = calculate_shark_length_from_pixel(pixel_length, original_width=longest_frame.shape[1], original_height=longest_frame.shape[0])
                     track['longest_length'] = segmentation_length
                     longest_length = track['longest_length']
 
@@ -1025,17 +1026,26 @@ class MainWindow(QMainWindow):
             self.frame_player.timer.stop()
 
             current_track = self.sorted_tracks[self.current_detection_index]
-            mask_overlay = current_track[1]['mask_overlay']
+            if 'mask_overlay' not in current_track[1]:
+                dlg = QMessageBox(self)
+                dlg.setWindowTitle("Alert")
+                dlg.setText("Error: No mask drawn for this track")
+                button = dlg.exec()
 
-            frame = mask_overlay
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            height, width, channel = frame_rgb.shape
-            bytes_per_line = 3 * width
-            q_image = QImage(frame_rgb.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
-            pixmap = QPixmap.fromImage(q_image)
-            scaled_pixmap = pixmap.scaled(self.frame_player.size(), Qt.AspectRatioMode.KeepAspectRatio)
-            
-            self.frame_player.setPixmap(scaled_pixmap)
+                if button == QMessageBox.StandardButton.Ok:
+                    print("OK!")
+
+            else:
+                mask_overlay = current_track[1]['mask_overlay']
+                frame = mask_overlay
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                height, width, channel = frame_rgb.shape
+                bytes_per_line = 3 * width
+                q_image = QImage(frame_rgb.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
+                pixmap = QPixmap.fromImage(q_image)
+                scaled_pixmap = pixmap.scaled(self.frame_player.size(), Qt.AspectRatioMode.KeepAspectRatio)
+                
+                self.frame_player.setPixmap(scaled_pixmap)
         else:
             self.frame_player.timer.start()
 
@@ -1428,7 +1438,112 @@ class FramePlayer(QLabel):
         # Show completion popup with both time and detections
         self.show_completion_popup(time_str, total_detections)
 
+class HeadlessVideoProcessor:
+    progress_update = 0
+    processing_complete = {}
+
+    def __init__(self, video_path, model, output_dir):
+        super().__init__()
+        self.video_path = video_path
+        self.model = model
+        self.output_dir = output_dir
+
+    def draw_bounding_boxes(self, frame, detections):
+        frame_with_boxes = frame.copy()
+        for x, y, w, h, confidence in detections:
+            cv2.rectangle(frame_with_boxes, 
+                          (int(x - w/2), int(y - h/2)), 
+                          (int(x + w/2), int(y + h/2)), 
+                          (0, 255, 0), 2)
+            label = f"Shark: {confidence:.2f}"
+            cv2.putText(frame_with_boxes, label, (int(x - w/2), int(y - h/2) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+        return frame_with_boxes
+
+    def run(self):
+        cap = cv2.VideoCapture(self.video_path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        
+        custom_tracker = CustomTracker()
+        
+        os.makedirs(os.path.join(self.output_dir, 'frames'), exist_ok=True)
+        os.makedirs(os.path.join(self.output_dir, 'bounding_boxes'), exist_ok=True)
+        os.makedirs(os.path.join(self.output_dir, 'false_positives'), exist_ok=True)
+
+        min_frame_skip, max_frame_skip = 10, 60
+        frame_skip = min_frame_skip
+        consecutive_empty_frames = 0
+        max_empty_frames = 1 * fps
+        detection_threshold = 0.4
+
+        frame_num = 0
+        while frame_num < total_frames:
+
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            results = self.model(frame, classes=[0], verbose=False)
+
+            detections = []
+            if results[0].boxes is not None and len(results[0].boxes) > 0:
+                boxes = results[0].boxes.xywh.cpu()
+                confidences = results[0].boxes.conf.cpu().tolist()
+
+                detections = [(float(x), float(y), float(w), float(h), confidence) 
+                               for (x, y, w, h), confidence in zip(boxes, confidences) 
+                               if confidence > detection_threshold]
+
+            has_detection = bool(detections)
+            if has_detection:
+                timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+                active_tracks = custom_tracker.update(detections, frame, timestamp)
+                
+                # Draw bounding boxes on the frame
+                frame_with_boxes = self.draw_bounding_boxes(frame, detections)
+                
+                # Emit the processed frame with bounding boxes
+                frame_processed = cv2.cvtColor(frame_with_boxes, cv2.COLOR_BGR2RGB)
+                
+                consecutive_empty_frames = 0
+                frame_skip = min_frame_skip
+            else:
+                # Emit the frame without bounding boxes
+                frame_processed = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                
+                consecutive_empty_frames += frame_skip
+
+            # Increase frame skip more aggressively
+            if consecutive_empty_frames >= max_empty_frames:
+                frame_skip = min(max_frame_skip, frame_skip * 2)
+
+            frame_num += frame_skip
+            self.progress_update = int((frame_num + 1) / total_frames * 100) 
+            print(self.progress_update)
+
+        cap.release()
+        
+        custom_tracker.save_best_frames(self.output_dir, self.video_path)
+        self.save_detections_csv(custom_tracker.tracks, self.output_dir)
+        self.processing_finished(custom_tracker.tracks)
+
+def mass_prediction(video_path, current_output_dir):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+    print(f"Using device: {device}")
+    model = YOLO(MODEL_PATH).to(device)
+    
+    processor = HeadlessVideoProcessor(video_path, model, current_output_dir)
+    processor.run()
+
 if __name__ == '__main__':
+    # video_path = Path("./TRIMMED_2023-04-23_Transect_DJI_0502.mp4")
+    # output_dir = Path("./headless_predictions")
+    
+    # results = mass_prediction(video_path=video_path, current_output_dir=output_dir)
+
+
     multiprocessing.freeze_support()
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(True)
